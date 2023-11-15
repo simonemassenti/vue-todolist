10aaf7d11755b978b7ec7cf8d4dad68eb0ba302c
simonemassenti <s.massenti96@gmail.com>
2023-11-15 15:54:07 +0100

    add: bonus 2 and 3

--- js/script.js
+++ js/script.js
@@ -3,17 +3,23 @@ const { createApp } = Vue;
 const app = createApp({
     data() {
         return {
-            newTask : "",
+            task : {
+                text : "",
+                done : false
+            },
             taskList : []
         }
     },
     methods : {
         getNewTask : function() {
-            this.taskList.push(this.newTask)
-            this.newTask = "";
+            this.taskList.push({...this.task});
+            this.task.text = "";
         },
         deleteTask : function(i) {
             this.taskList.splice(i,1);
+        },
+        doneUndone : function(i) {
+            this.taskList[i].done = !this.taskList[i].done;
         }
     }
 }).mount("#app")
